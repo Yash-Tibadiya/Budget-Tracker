4c9788db3fb57c417ab294105fb8cbacd22ef73a
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-06-02 13:08:13 +0530

    feat: implement CategoriesStats component for displaying categorized financial statistics; integrate it into Overview component

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -7,6 +7,7 @@ import { differenceInDays, startOfMonth } from "date-fns";
 import { useState } from "react";
 import { toast } from "sonner";
 import StatsCards from "./StatsCards";
+import CategoriesStats from "./CategoriesStats";
 
 const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -47,6 +48,11 @@ const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
           to={dateRange.to}
         />
 
+        <CategoriesStats
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
       </div>
     </>
   );
