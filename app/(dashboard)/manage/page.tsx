60154ff70dfe1f0de8f78a8f933a0cdd00fb64ff
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-06-02 18:03:38 +0530

    feat: Implement transaction history API and UI enhancements
    - Added API endpoint for fetching transaction history with date filtering.
    - Created DataTable components for displaying transactions with sorting and filtering capabilities.
    - Implemented column visibility toggle and faceted filters for better data management.
    - Added delete transaction functionality with confirmation dialog.
    - Enhanced UI with improved styling and user feedback through toasts.
    - Updated package dependencies to include new libraries for data handling and UI components.

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -128,7 +128,7 @@ function CategoryList({ type }: { type: TransactionType }) {
 
 function CategoryCard({ category }: { category: Category }) {
   return (
-    <div className="flex border-separate flex-col justify-between rounded-md border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1] ">
+    <div className="flex border-separate flex-col justify-between rounded-md border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1] mb-24 ">
       <div className="flex flex-col items-center gap-2 p-4 ">
         <span className="text-3xl " role="img">
           {category.icon}
